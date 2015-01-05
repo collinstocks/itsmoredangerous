a5420983bff132080d97c86f2193d5dd2bce4d89
Collin RM Stocks <collinstocks@gmail.com>
2015-01-05 00:12:28 -0500

    Start writing a bit stream class for better serialization.

--- itsmoredangerous/_utils.py
+++ itsmoredangerous/_utils.py
@@ -1,5 +1,6 @@
 from datetime import datetime, timedelta
-import struct
+from bitarray import bitarray
+
 
 
 class Object (dict):
@@ -10,12 +11,40 @@ class Object (dict):
         self.__dict__ = state
 
 
+
 class Enum (Object):
     def __init__ (self, *args, **kwds):
         Object.__init__(self, zip(args, range(len(args))), **kwds)
         self.__dict__ = self
     def __setstate__ (self, state):
         self.__dict__ = state
+
+
+
+class BitStream (object):
+
+    def __init__ (self, f):
+        self.__f = f
+        self.__buf = bitarray()
+        self.__entered = False
+
+    def __enter__ (self):
+        self.__entered = True
+
+    def __exit__ (self):
+        self.__f.write(self.__buf.tobytes())
+
+    def write (self, s, delta_bits=None):
+        self.__buf.frombytes(s)
+        for i in range(delta_bits):
+            self.__buf.pop()
+        flush_len = self.__buf.length() // 8
+        self.__f.write(self.__buf.tobytes()[:flush_len])
+        self.__buf = self.__buf[8*flush_len:]
+
+    def read (self, bytes=0, bits=0):
+        pass
+
 
 
 def now ():
